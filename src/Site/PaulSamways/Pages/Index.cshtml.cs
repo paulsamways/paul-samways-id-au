569fbff99cb57aedd4609b1f2fcde6f074f4fb49
Paul Samways <paul@samways.id.au>
2022-09-15 07:30:43 +1000

    Resume is now on it's own page, and the root just redirects. Print style improvements

--- src/Site/PaulSamways/Pages/Index.cshtml.cs
+++ src/Site/PaulSamways/Pages/Index.cshtml.cs
@@ -1,29 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using PaulSamways.Models;
-using PaulSamways.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PaulSamways.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-    private readonly DataService _data;
-
-    public IndexModel(ILogger<IndexModel> logger, DataService data)
+    public IActionResult OnGet()
     {
-        _logger = logger;
-        _data = data;
-
-        Positions = Array.Empty<Position>();
-    }
-
-    public Position[] Positions { get; set; }
-
-    public Skill Skills { get; set; }
-
-    public void OnGet()
-    {
-        Positions = _data.Positions.ToArray();
-        Skills = _data.Skills;
+        return RedirectToPage("/Resume");
     }
 }
---
+++ src/Site/PaulSamways/Pages/Resume.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using PaulSamways.Models;
+using PaulSamways.Services;
+
+namespace PaulSamways.Pages;
+
+public class ResumeModel : PageModel
+{
+    private readonly ILogger<ResumeModel> _logger;
+    private readonly DataService _data;
+
+    public ResumeModel(ILogger<ResumeModel> logger, DataService data)
+    {
+        _logger = logger;
+        _data = data;
+
+        Positions = Array.Empty<Position>();
+    }
+
+    public Position[] Positions { get; set; }
+
+    public Skill Skills { get; set; }
+
+    public void OnGet()
+    {
+        Positions = _data.Positions.ToArray();
+        Skills = _data.Skills;
+    }
+}
